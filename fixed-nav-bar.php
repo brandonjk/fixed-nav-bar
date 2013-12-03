f03bce874bfc443d6cf11a838677b1ce6833f8ab
Brandon Kwong <brandonjk@gmail.com>
2013-12-03 00:12:51 -0800

    File additions and organization

---
+++ fixed-nav-bar.php
@@ -0,0 +1,48 @@
+<!-- Nav-Bar -->
+<div id="nav-bar">
+    <div id="nav-container">
+        <div id="nav-header">
+            <a href="#"><h1>fixed nav-bar</h1></a>
+            <!-- Note: you can also place your logo image here
+            <a href="#"><img src="logo.png" width="100%" alt="logo"></a>
+            -->
+        </div>
+    <!-- Menu Primary -->
+        <div id="nav-menu">
+            <ul>
+                <li><a href="#" id="item-1">About</a></li>
+                <li><a href="#" id="item-2">Explore</a>
+                <!-- Secondary Menu -->
+                    <div class="arrow"></div>
+                    <ul>
+                        <li><a href="#">Africa</a></li>
+                        <li><a href="#">North America</a></li>
+                        <li><a href="#">South America</a></li>                        
+                        <li><a href="#">Antarctica</a></li>
+                        <li><a href="#">Asia</a></li>
+                        <li><a href="#">Australia</a></li>
+                        <li><a href="#">Europe</a></li>
+                    </ul>
+                </li>
+                <li><a href="#" id="item-3">Contact</a></li>
+            </ul>
+        </div>
+    <!-- Menu Right -->
+        <div id="nav-menu-r">
+            <ul>
+                <li>
+                <!-- Search Bar -->
+                    <div id="search-bar">
+                        <form role="search" method="get" id="" action="">
+                        <input type="text" value="" name="" id="" placeholder="Search" />
+                        <input type="image" src="images/search-icon-small.png" class="search-icon" alt="submit">
+                        </form>
+                    </div>
+                </li>
+                <li><a href="#">Sign In</a></li>
+            </ul>
+        </div>
+    </div>    
+</div>
+<!-- Nav-Back -->
+<div id="nav-back"></div>
